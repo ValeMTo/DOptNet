44afc93fee43a6f9d7350f4f35b05733c5336bd8
ValeMTo <valeria.amato@polimi.it>
2025-03-06 16:04:17 +0100

    fix bugs in local parsers

--- translation/parsers/localDataParser.py
+++ translation/parsers/localDataParser.py
@@ -5,10 +5,10 @@ class localDataParserClass:
         self.logger = logger
         self.logger.info("Local Data parser initialized")
     
-    def get_powerplants_data(self, countries):
+    def get_already_installed_powerplants_data(self, countries):
         self.logger.debug("Getting powerplants data")
 
-        data = pd.read_csv('../data/custom_powerplants_ssp126_2050.csv')
+        data = pd.read_csv('./data/custom_powerplants_ssp126_2050.csv', index_col=0)
 
         filtered_data = data[data['DateIn'] <= 2020]
         filtered_data = filtered_data[filtered_data['DateOut'] >= 2030]
@@ -28,15 +28,15 @@ class localDataParserClass:
         if not isinstance(countries, list) or not all(isinstance(country, str) and len(country) == 2 for country in countries):
             raise ValueError("Countries must be a list of strings with exactly 2 characters each")
 
-        df = pd.read_csv('../data/demand_TEMBA_SSP1-2.6.csv')
+        df = pd.read_csv('./data/demand_TEMBA_SSP1-2.6.csv', index_col=0, sep=';')
 
         df = df.map(lambda x: round((x/(365*24*60*60))*10**6, 3)) # Convert from PJ/year to GW
 
         df['country'] = df.index.map(lambda x: x[:2])
-        filtered_data = df[df['Country'].isin(countries)]
+        filtered_data = df[df['country'].isin(countries)]
         filtered_data = filtered_data[[str(year), 'country']]
         filtered_data.reset_index(drop=True, inplace=True)
-        filtered_data = filtered_data.rename(columns={str(year): 'annual_demand_{year}_GW'})
+        filtered_data = filtered_data.rename(columns={str(year): f'annual_demand_{year}_GW'})
 
         return filtered_data
 
